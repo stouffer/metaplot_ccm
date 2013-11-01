d1108d65ecd05fa5322507213bcc494e545a4a66
adamtclark <adam.tclark@gmail.com>
2013-11-01 18:44:58 -0500

    Runs most of the existing code - still need to average library before CCM

--- RUN_CCM_multi_Library.R
+++ RUN_CCM_multi_Library.R
@@ -2,15 +2,20 @@ setwd("~/Dropbox/Active Work/Sugihara/Metacommunity_CCM/")
 source("CCM_multi_Library.R")
 
 system.time({
+#Initialize and make data
 program_init_bootstrap()
-ode_result<-make_comp_data_boot(seednum=1000, xstr=0.5,  times=seq(1, 50, by = 1), x_mean_sd = c(25, 1), y_mean_sd = c(5, 1), number_of_chains=5)
+ode_result<-make_comp_data_boot_Cfxn(seednum=1000, xstr=1,  times=seq(1, 500, by = 1), number_of_chains=50)
 plot_output_boot(ode_result)
 
+#Average data to observed level
+
+
 #Compare species to environment
-eplot_out_boot<-makeEplot_environment_boot(ode_result, "all", tau=1, predstep=tau, maxE=5)
-ccm_out<-doCCM_environment(ode_result, "all", tau=1, maxE=5, iterations=100)
+eplot_out_boot<-makeEplot_environment_boot(ode_result, "all", tau=1, predstep=1, maxE=5)
+ccm_out<-doCCM_environment(ode_result=ode_result, target_sp="all", predstep=1, tau=1, maxE=5, iterations=100, twoway=FALSE)
 ssr_out<-ssr_data(ccm_out, predstepmax=10, tau=1)
-plot_ccm(ccm_out,  ylimits=c(-0.2, 1))
+plot_ccm(ccm_out,  ylimits=c(-0.1, 0.4), twoway=FALSE)
+})
 
 ####
 The below still need to be fixed
