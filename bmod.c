86181462de11ec4cce43883f8ba5758ee8d659b8
adamtclark <adam.tclark@gmail.com>
2013-11-01 18:45:22 -0500

    Function now seems to work - computes based on two external forcing functions

--- bmod.c
+++ bmod.c
@@ -2,6 +2,8 @@
 #include <R.h>
 #include <Rmath.h>
 static double parms[35];
+static double forc[2];
+
 #define OUx parms[0]
 #define OUy parms[1]
 #define Wx parms[2]
@@ -17,79 +19,78 @@ static double parms[35];
 #define K parms[12]
 #define w parms[13]
 // 12 species - parms[14-25] are xopt, and parms[26-37] are yopt
+#define temp forc[0]
+#define precip forc[1]
+
 /* initializer */
-void initmod(void (* odeparms)(int *, double *)) {
+void parmsc(void (* odeparms)(int *, double *)) {
   int N=38;
   odeparms(&N, parms);
 }
 
-/* Derivatives and 1 output variable */
+void forcc(void (* odeforcs)(int *, double *)) {
+  int N=2;
+  odeforcs(&N, forc);
+}
+
+/* Derivatives and output variables */
 void derivs (int *neq, double *t, double *y, double *ydot, double *yout, int *ip) {
 GetRNGstate();
   double gix; //Growth rates for each species
   double rgain;
   double rtotal;
-  int xoptstart = 14, yoptstart = 26;
-  int spstart = 3;
+  const int xoptstart = 14, yoptstart = 26;
+  const int spstart = 1;
+  const int respos = 0;
   
-  if (ip[0] <14) error("nout should be at least 14");
-  //Climate variables
-  ydot[0] = (midx-y[0])*OUx+rnorm(0, Wx);
-  ydot[1] = (midy-y[1])*OUy+rnorm(0, Wy);
-    
+  if (ip[0] <13) error("nout should be at least 13");
+
   //Species and resources
   rtotal = 0; //Sum growth for resource change
+  //fprintf(stderr, "temp = %.1f\n", forc[0]);
+  //fprintf(stderr, "precip = %.1f\n", forc[1]);
   for(unsigned int i = 0; i < 12; i++) { // 12 species
-    gix = r*exp(-0.5*pow((abs(parms[xoptstart+i]-y[0])*(xstr)+abs(parms[yoptstart+i]-y[1])*(1-xstr))/w, 2)); //optimum-dependent growth rate
+    if(y[spstart+i] > 0.0001) {
+    gix = r*exp(-0.5*pow((abs(parms[xoptstart+i]-temp)*(xstr)+abs(parms[yoptstart+i]-precip)*(1-xstr))/w, 2)); //optimum-dependent growth rate
 
     //Species increase and decrease
     yout[i] = gix*y[spstart+i];
-    rgain = yout[i]*y[2]/(y[2]+K); //Increase in species i
+    rgain = yout[i]*y[respos]/(y[respos]+K); //Increase in species i
     rtotal = rtotal + yout[i]; //Sum growth for resource change
     ydot[spstart+i] = rgain-m*y[spstart+i]; //Total growth in species i
+    } else {
+      y[spstart+i] = 0;
+      gix = 0.0; //optimum-dependent growth rate
+      //Species increase and decrease
+      yout[i] = 0;
+      rgain = 0.0; //Increase in species i
+      ydot[spstart+i] = 0.0; //Total growth in species i
+    }
+    
   }
-  ydot[2] = a*(S-y[2])-Q*rtotal*y[2]/(y[2]+K); //Change in resource
+  ydot[respos] = a*(S-y[respos])-Q*rtotal*y[respos]/(y[respos]+K); //Change in resource
   
   yout[12] = rtotal;
-  yout[13] = gix;
-  
 PutRNGstate();
 }
 
 /* The Jacobian matrix */
 void jac(int *neq, double *t, double *y, int *ml, int *mu, double *pd, int *nrowpd, double *yout, int *ip) {
-const double gix = yout[13];
-const double gixR = gix*y[2]/(y[2]+K);
-const double gixRm = gix*y[2]/(y[2]+K)-m;
-const double QgixR = Q*gix*y[2]/(y[2]+K);
-const double devR = K/(K + pow(y[2],2));
 unsigned int i;
+const int spstart = 1;
+const int respos = 0;
 
-  // col 1, Climate x
-  pd[0] = -OUx;
-    //for(i = 1; i < (*nrowpd); i++) {
-    //  pd[i] = 0.0;
-    //}
-    
-  // col 2, Climate y
-    //pd[(*nrowpd)] = 0.0;
-  pd[(*nrowpd)+1] = -OUy;
-    //for(i = (*norwpd)+2; i < 2*(*nrowpd); i++) {
-      //pd[i] = 0.0;
-    //}
-    
-  // col 3, Resource
-    //pd[2*(*nrowpd)] = 0.0;
-    //pd[2*(*nrowpd)+1] = 0.0;
-  pd[2*(*nrowpd)+2] = a - devR*Q*yout[12];
-  for(i = 2*(*nrowpd)+3; i < 3*(*nrowpd); i++) {
-    pd[i] = -QgixR;
+ // col 0, Resource
+  pd[0] = (Q*y[respos]*yout[12])/pow(K + y[respos],2) - (Q*yout[12])/(K + y[respos]) - a;
+  unsigned int n = 0;
+  for(i = 1; i < (*nrowpd); i++) {
+    pd[i] = (Q*yout[n])/(K + y[respos]) - (Q*y[respos]*yout[n])/pow(K + y[respos],2);
+    n = n+1;
   }
-  
-  // col 4+, Species
-  for(i = 3; i < 15; i++) {
-    pd[i*(*nrowpd)+2] = yout[i-3]*devR; //dSi/dR
-    pd[i*(*nrowpd)+i] = gixRm; //dSi/dSi
+  // col 1+, Species
+  for(i = 0; i < 12; i++) {
+    pd[(i+spstart)*(*nrowpd)] = (K*yout[i])/pow(K + y[respos],2); //dSi/dR
+    pd[(i+spstart)*(*nrowpd)+i+spstart] = (y[respos]*yout[i]/y[spstart+i])/(K + y[respos]) - m; //dSi/dSi
   }
 }
 /* END file mymod.c */
